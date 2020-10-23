31e0d83cc7d97410e746adeed497fdeb5deeb831
Mattia <mattiapizzolitto141@gmail.com>
2020-10-23 22:11:11 +0200

    Placeholder code

--- src/com/battleship/GUI/Window.java
+++ src/com/battleship/GUI/Window.java
@@ -22,11 +22,24 @@ public class Window extends JFrame {
      */
     private void initUI() {
 
-        //pack()
-        setMinimumSize(new Dimension(300, 300));
 
         setTitle("BattleShip Game");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(800, 800));
+
+        Panel pnl = new Panel();
+        this.add(pnl);
+
+        Button b_start = new Button("Start");
+        Button b_exit = new Button("Exit");
+        TextField tf_name = new TextField("Enter your name");
+
+        pnl.add(b_start);
+        pnl.add(b_exit);
+        pnl.add(tf_name);
+
+        pack();
         setLocationRelativeTo(null);
+        setVisible(true);
     }
 }
