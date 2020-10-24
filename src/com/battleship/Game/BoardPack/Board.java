728658f9ca03b27498ebc2ad8fbf07c4a45cf710
Kevin Chierchia <kevchi9@yahoo.it>
2020-10-24 12:27:26 +0200

    Modified Ship class and Board class for the initial setup of the game

--- src/com/battleship/Game/BoardPack/Board.java
+++ src/com/battleship/Game/BoardPack/Board.java
@@ -1,17 +1,50 @@
 package com.battleship.Game.BoardPack;
 
 import com.battleship.Game.ShipPack.Ship;
+import com.battleship.Game.PlayerPack.Player;
 
 public class Board {
 
-    private Ship[] field_player_1;
-    private Ship[] Field_player_2;
+    private Ship[] field;
+    private int ships_on_field = 0;
+
 
     /**
      * Constructor of Board
      */
     public Board() {
-
+        field = new Ship[10];
     }
 
+    public void addShip(Ship ship){
+        field[ships_on_field] = ship;
+        ship.setPosition(ships_on_field);
+        ships_on_field++;
+    }
+    /**
+     * 
+     * @param position = the ship's position in the field ship's array
+     * @param ship = the new ship to overwrite on the previous value of field
+     */
+    public void modifyBoard(int position, Ship ship){ 
+        field[position] = ship;
+    }
+
+/*
+    private ShipUnit[] makeUnitList(int x1, int y1, int x2, int y2) {
+
+        ShipUnit[] list = new ShipUnit[length];
+
+        if (x1 - x2 == 0) { // vertical ship
+            for (int i = 0; i < length; i++) {
+                list[i] = new ShipUnit(x1, y1 + i);
+            }
+        } else {            // horizontal ship
+            for (int i = 0; i < length; i++) {
+                list[i] = new ShipUnit(x1 + i, y1);
+            }
+        }
+        return list;
+    }
+    */
 }
